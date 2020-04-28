108fa4544b0a7222e5f8d1da5964fc8adba9f7a2
mihajlo-perendija <54019549+mihajlo-perendija@users.noreply.github.com>
2020-04-28 20:19:48 +0200

    packages update and header template

--- MainApp/Client/src/app/app.module.ts
+++ MainApp/Client/src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [CookieService],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HeaderComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [
+      CookieService
+   ],
+   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
