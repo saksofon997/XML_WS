4a59f91178af8964d34d722c39036e90311d8b2e
saksofon997 <saksofon997@gmail.com>
2020-04-29 14:48:02 +0200

    Fix header, add administration, work on side list

--- MainApp/Client/src/app/app-routing.module.ts
+++ MainApp/Client/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdministrationMainComponent } from './administration/administration-main/administration-main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "administration", component: AdministrationMainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+/*Add all routing objects to this array*/
+export const routingComponents = [AdministrationMainComponent]
--- MainApp/Client/src/app/app.module.ts
+++ MainApp/Client/src/app/app.module.ts
@@ -2,14 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SideListComponent } from './administration/side-list/side-list.component';
+import { AdminTableComponent } from './administration/admin-table/admin-table.component';
+
+const AdministrationComponents = [SideListComponent, AdminTableComponent]
 
 @NgModule({
    declarations: [
       AppComponent,
-      HeaderComponent
+      HeaderComponent,
+      routingComponents,
+      AdministrationComponents
+
    ],
    imports: [
       BrowserModule,
