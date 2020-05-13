974d9ab7ec0f2684c6c3415e4879bbc71cbf9c37
saksofon997 <saksofon997@gmail.com>
2020-05-13 17:54:28 +0200

    Initial servers

---
+++ MainApp/Servers/xmlws/src/main/java/com/java/xmlws/XmlwsApplication.java
@@ -0,0 +1,13 @@
+package com.java.xmlws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlwsApplication.class, args);
+	}
+
+}
---
+++ MainApp/Servers/xmlws/src/test/java/com/java/xmlws/XmlwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.xmlws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
