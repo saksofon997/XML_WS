72c4afe43b4b9aa594cb81664e2e7e64df20ba01
unknown <petrosbasic@gmail.com>
2020-05-15 13:44:49 +0200

    TECh-2: Correct docker-compose setup.

--- MainApp/Servers/xmlws/src/main/java/com/java/xmlws/XmlwsApplication.java
+++ MainApp/Servers/xmlws/src/main/java/com/java/xmlws/XmlwsApplication.java
@@ -2,8 +2,10 @@ package com.java.xmlws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class XmlwsApplication {
 
 	public static void main(String[] args) {
