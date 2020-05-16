9858f44f234b6578e219d86a064ac2c76fc60864
unknown <petrosbasic@gmail.com>
2020-05-16 16:09:40 +0200

    TECH-3: Mark services as eureka clients.

--- MainApp/Servers/rental/src/main/java/rental/RentalApplication.java
+++ MainApp/Servers/rental/src/main/java/rental/RentalApplication.java
@@ -2,8 +2,10 @@ package rental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RentalApplication {
 
 	public static void main(String[] args) {
--- MainApp/Servers/vehicle/src/main/java/vehicle/VehicleApplication.java
+++ MainApp/Servers/vehicle/src/main/java/vehicle/VehicleApplication.java
@@ -2,8 +2,10 @@ package vehicle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VehicleApplication {
 
 	public static void main(String[] args) {
