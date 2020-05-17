2a8fbbf42206d7a82d4464282c7ebd12be1fe0f4
unknown <petrosbasic@gmail.com>
2020-05-17 02:31:36 +0200

    master: Maybe final services setup (depend??).

--- MainApp/Servers/location/src/main/java/location/LocationApplication.java
+++ MainApp/Servers/location/src/main/java/location/LocationApplication.java
@@ -2,10 +2,8 @@ package location;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class LocationApplication {
 
 	public static void main(String[] args) {
