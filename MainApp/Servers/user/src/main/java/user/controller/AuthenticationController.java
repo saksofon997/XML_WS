3110f28246757b8445821653becb0fe0a7886eea
unknown <petrosbasic@gmail.com>
2020-05-29 13:35:28 +0200

    TECH-3: Fix zuul filter routing.

---
+++ MainApp/Servers/user/src/main/java/user/controller/AuthenticationController.java
@@ -0,0 +1,103 @@
+package user.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+import user.dto.UserDTO;
+import user.model.User;
+import user.repository.UserRepository;
+import user.security.JwtAuthenticationRequest;
+import user.security.TokenUtils;
+import user.service.AuthUserDetailsService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+@RequestMapping(value = "/auth", produces = MediaType.APPLICATION_JSON_VALUE)
+public class AuthenticationController {
+
+    @Autowired
+    TokenUtils tokenUtils;
+
+    @Lazy
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private AuthUserDetailsService authUserDetailsService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtAuthenticationRequest authenticationRequest,
+                                                       HttpServletResponse response) throws AuthenticationException, IOException {
+
+        Authentication authentication = null;
+        try {
+            authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getEmail(),
+                            authenticationRequest.getPassword()));
+        } catch (Exception e){
+            return new ResponseEntity<>("Bad credentials", HttpStatus.BAD_REQUEST);
+        }
+
+        // Ubaci username + password u kontext
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        // Kreiraj token
+        //User user = (User) authentication.getPrincipal();
+        Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByEmail(loggedInUser.getName());
+
+        String jwt = tokenUtils.generateToken(user.getEmail());
+        int expiresIn = tokenUtils.getExpiredIn();
+
+        // Provera promene passworda pri prvom loginu
+        UserState userState = new UserState();
+        userState.token = jwt;
+        userState.user = new UserDTO(user);
+
+        // Vrati token kao odgovor na uspesno autentifikaciju
+        return ResponseEntity.ok(userState);
+    }
+
+    @RequestMapping(value = "/verify/{token}", method = RequestMethod.GET)
+    public ResponseEntity<?> verify(@PathVariable("token") String token){
+        String email = tokenUtils.getEmailFromToken(token);
+        if (email == null){
+            return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
+        }
+        UserDetails user = authUserDetailsService.loadUserByUsername(email);
+        if (user == null){
+            return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
+        }
+
+        if (tokenUtils.validateToken(token, user)) {
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
+        }
+    }
+    @GetMapping(path = "/test",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getClinic() {
+        return "CAO JA SAM USER SERVIS izmenjen doduse drugi put";
+    }
+
+    static class UserState {
+        public String token;
+        //public UserDTO user;
+        public UserDTO user;
+    }
+}
---
+++ MainApp/Servers/zuul/src/main/java/com/spring/zuul/filter/AuthFilter.java
@@ -0,0 +1,69 @@
+package com.spring.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class AuthFilter extends ZuulFilter {
+
+    private final String HEADER = "x-auth";
+    private final String PREFIX = "Bearer ";
+
+    @Autowired
+    private AuthClient authClient;
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 10;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        String authenticationHeader = request.getHeader(HEADER);
+        if (authenticationHeader == null || !authenticationHeader.startsWith(PREFIX)){
+            System.out.println( "No header present");
+            return null;
+        }
+
+        String jwtToken = authenticationHeader.replace(PREFIX, "");
+        System.out.println(jwtToken);
+
+        try {
+            boolean valid = authClient.verify(jwtToken);
+            System.out.println(valid);
+            // redirection (?) -> not needed I fixed it... :)
+        } catch (Exception e) {
+            setFailedRequest("Invalid token", 403);
+        }
+
+        return null;
+    }
+
+    private void setFailedRequest(String body, int code) {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        ctx.setResponseStatusCode(code);
+        if (ctx.getResponseBody() == null) {
+            ctx.setResponseBody(body);
+            ctx.setSendZuulResponse(false);
+        }
+    }
+}
