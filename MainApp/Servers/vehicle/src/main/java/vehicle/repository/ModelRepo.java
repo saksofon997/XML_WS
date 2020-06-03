694216439b0522dc4e18f4369c5363abd0e7d003
Aleksandar <saksofon997@gmail.com>
2020-06-03 20:07:49 +0200

    fix pageable

--- MainApp/Servers/vehicle/src/main/java/vehicle/repository/ModelRepo.java
+++ MainApp/Servers/vehicle/src/main/java/vehicle/repository/ModelRepo.java
@@ -1,6 +1,7 @@
 package vehicle.repository;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import vehicle.model.Brand;
 import vehicle.model.Model;
@@ -8,5 +9,5 @@ import vehicle.model.Model;
 public interface ModelRepo extends JpaRepository<Model, Long> {
     boolean existsByName(String name);
 
-    Page<Model> findByBrand(Brand brand);
+    Page<Model> findByBrand(Brand brand, Pageable page);
 }
