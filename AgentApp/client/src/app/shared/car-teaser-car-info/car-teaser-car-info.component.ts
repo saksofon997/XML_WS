ab15e3ca763b278587940afb7d2d98b3b7f08bfd
Aleksandar <saksofon997@gmail.com>
2020-06-16 19:55:35 +0200

    Add manual occupancy both fronts

--- AgentApp/client/src/app/shared/car-teaser-car-info/car-teaser-car-info.component.ts
+++ AgentApp/client/src/app/shared/car-teaser-car-info/car-teaser-car-info.component.ts
@@ -3,6 +3,9 @@ import { Car } from 'src/app/models/Car.model';
 import { environment } from 'src/environments/environment';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogBoxComponent } from 'src/app/administration/dialog-box-edit/dialog-box-edit.component';
+import { OccupancyDialogBoxComponent } from 'src/app/vehicle-page/occupancy-dialog-box/occupancy-dialog-box.component';
+import { VehicleOccupancy } from 'src/app/models/VehicleOccupancy.model';
+import { OccupancyService } from 'src/app/services/occupancy.service';
 
 @Component({
   selector: 'app-car-teaser-car-info',
@@ -15,28 +18,40 @@ export class CarTeaserCarInfoComponent implements OnInit {
   @Input() admin: boolean;
   API_URL = environment.API_URL;
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog,
+    public occupancyService: OccupancyService) { }
 
   ngOnInit() {
   }
 
   openDialog(action, obj) {
     obj.action = action;
-    const dialogRef = this.dialog.open(DialogBoxComponent, {
-      width: '300px',
+    const dialogRef = this.dialog.open(OccupancyDialogBoxComponent, {
+      width: '400px',
       data: obj
     });
 
     dialogRef.afterClosed().subscribe(result => {
       if (result.event === 'Update') {
-        console.log(result.data); //TODO for manual occupancy
+
+        let occupancy = new VehicleOccupancy(null, Number(result.data.from) / 1000, Number(result.data.to) / 1000, "MANUAL", null);
+
+        this.occupancyService.add(this.car?.id, occupancy).subscribe(
+          (data: any) => {
+            console.log(data);
+          },
+          (error) => {
+            alert(error);
+          }
+        );
+
       } else if (result.event === 'Delete') {
         console.log(result.data); //TODO
       }
     });
   }
 
-  checkMileage(mileage){
+  checkMileage(mileage) {
     return mileage != -1 ? mileage : "Unlimited";
   }
 
--- MainApp/Client/src/app/shared/shared.module.ts
+++ MainApp/Client/src/app/shared/shared.module.ts
@@ -7,6 +7,16 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { ReviewComponent } from './review/review.component';
 import { CarTeaserCarInfoComponent } from './car-teaser-car-info/car-teaser-car-info.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MaterialFileInputModule } from 'ngx-material-file-input';
 
 // import this module in all others which use these shared components and modules
 @NgModule({
@@ -22,7 +32,19 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MatIconModule,
     MatTooltipModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+
+    //material
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatTableModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MaterialFileInputModule,
   ],
   exports: [
     // export all of the above
