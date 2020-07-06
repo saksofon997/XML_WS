f3d566b751e5dedc94b067481aec16093236b16e
Aleksandar <saksofon997@gmail.com>
2020-07-06 16:44:59 +0200

    Fix date view on pending reviews

--- AgentApp/client/src/app/services/token.interceptor.ts
+++ AgentApp/client/src/app/services/token.interceptor.ts
@@ -18,7 +18,8 @@ export class TokenInterceptor implements HttpInterceptor {
         const login = /login/gi;
         const yandex = /apikey/gi; 
         const register = /register/gi;
-        if (request.url.search(login) === -1 && request.url.search(yandex) === -1 && request.url.search(register) === -1) {
+        const locate = /locate/gi;
+        if (request.url.search(login) === -1 && request.url.search(yandex) === -1 && request.url.search(register) === -1 && request.url.search(locate) === -1) {
             request = request.clone({
                 setHeaders: {
                     'x-auth': `Bearer ${this.userService.getToken()}` //${token}
