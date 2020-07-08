8715decdf96ce1be7e900ccf5005946b533d187c
Aleksandar <saksofon997@gmail.com>
2020-07-08 17:33:05 +0200

    Discounted price and mileage penalty

--- MainApp/Client/src/app/shared/new-rental-report-dialogbox/new-rental-report-dialogbox.component.ts
+++ MainApp/Client/src/app/shared/new-rental-report-dialogbox/new-rental-report-dialogbox.component.ts
@@ -1,6 +1,7 @@
 import { Component, Optional, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { RentalReport } from 'src/app/models/RentalReport.model';
+import { RentalFront } from 'src/app/models/Rental.model';
 
 @Component({
   selector: 'app-new-rental-report-dialogbox',
@@ -9,13 +10,53 @@ import { RentalReport } from 'src/app/models/RentalReport.model';
 })
 export class NewRentalReportDialogboxComponent {
 
-  local_data: RentalReport;
+  local_data: any;
+  report: RentalReport;
+  rental: RentalFront;
+
+  penalty: boolean = false;
+  mileage: any = 0;
+  penaltyValue: any;
 
   constructor(
     public dialogRef: MatDialogRef<NewRentalReportDialogboxComponent>,
     // @Optional() is used to prevent error if no data is passed
-    @Optional() @Inject(MAT_DIALOG_DATA) public data: RentalReport) {
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: any) {
     this.local_data = { ...data };
+
+    this.report = this.local_data.report;
+    this.rental = this.local_data.rental;
+  }
+
+  calculatePenalty(event) {
+    this.mileage = event.target.value;
+    
+    if((this.mileage > this.rental.car.availableMileage) && this.rental.car.availableMileage != -1) {
+      this.penalty = true;
+      this.penaltyValue = (this.mileage - this.rental.car.availableMileage) * this.rental.car.pricelist.penalty;
+    }
+    else {
+      this.penalty = false;
+      this.penaltyValue = null;
+    }
+  }
+
+  getPenalty() {
+    return this.penaltyValue;
+  }
+
+  finalPrice() {
+    let duration = this.rental.to - this.rental.from;
+
+    let days = Math.ceil(duration/(60*60*24));
+
+    let price =  days * this.rental.car.pricelist.pricePerDay;
+
+    if(days > 5) {
+      price = price -  price * this.rental.car.pricelist.discount;
+    }
+
+    return price;
   }
 
   doAction() {
